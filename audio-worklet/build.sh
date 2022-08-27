69dcca44ab3d519a8fc364ef39c1d6c0d3ae7ab7
Marcel Gleeson <gleeson.marcel@gmail.com>
2022-08-27 16:14:59 +0200

    Fix build

--- audio-worklet/build.sh
+++ audio-worklet/build.sh
@@ -4,15 +4,15 @@ set -ex
 
 # Full build
 
-# Build type definitions for interfaces
-npm run build:types
-
 # Remove the previous build artifacts.
 rm -rf dist pkg
 
 # Create the build directory.
 mkdir -p dist/src/worklet
 touch dist/src/worklet/sobaka.worklet.js
+
+# Build type definitions for interfaces
+npm run build:types
 
 # First build with dummy worklet to generate the proper WASM TypeScript definitions
 npm run build:wasm
